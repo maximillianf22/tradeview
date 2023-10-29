87cce71dec8f5ba82688881f17e72c2a8fc706bd
Maximiliam Fernandez <you@example.com>
2023-10-29 03:34:11 -0500

    Feature: Add translations on call actions communiTraders

--- components/Home/CommuniTradersSection/molecules/CommuniTradersSection.vue
+++ components/Home/CommuniTradersSection/molecules/CommuniTradersSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-secondary py-20 text-center">
     <h1 class="text-xl lg:text-2xl font-bold text-white">
-      Try CommuniTraders Now!
+      {{ $t("try-communi-traders-now") }}
     </h1>
     <call-action-buttons />
   </div>
--- components/Home/ToolsSection/molecules/ToolsSection.vue
+++ components/Home/ToolsSection/molecules/ToolsSection.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="bg-[#f2f2f2] dark:bg-secondary px-3 py-10 lg:px-10">
+  <div class="bg-[#f2f2f2] dark:bg-[#202a37] px-3 py-10 lg:px-10">
     <div
       class="container mx-auto flex items-center flex-col lg:flex-row text-secondary dark:text-white"
     >
       <div class="w-full lg:w-1/2 text-center lg:text-start mb-5 lg:mb-0">
         <h1 class="text-xl lg:text-2xl font-bold">
-          {{ $t("leading-edge-technology") }}
+          {{ $t("tools.leading-edge-technology") }}
         </h1>
         <p class="text-md lg:text-xl font-light my-4 lg:max-w-[75%]">
-          {{ $t("easy-to-use-management-tools") }}
+          {{ $t("tools.easy-to-use-management-tools") }}
         </p>
         <a href="" class="text-sm underline font-light">
-          {{ $t("quick-guide-link") }}
-          <span v-html="$t('external-link-icon')"></span>
+          {{ $t("tools.quick-guide-link") }}
+          <span v-html="$t('tools.external-link-icon')"></span>
         </a>
       </div>
       <div class="w-full lg:w-1/2">
